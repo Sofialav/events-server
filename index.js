79bc9ab156cccba94d41e22dbeca26c4370fea22
Sofialav <cemeteryvixen@gmail.com>
2020-02-27 15:27:28 +0100

    middlw inst-d, event router crud

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const db = require("./db");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const eventRouter = require("./Event/router");
 
 const app = express();
+const corsMiddleware = cors();
+const parserMiddleware = bodyParser.json();
 const port = 4000;
+
+app.use(corsMiddleware);
+app.use(parserMiddleware);
+app.use(eventRouter);
 
 app.listen(port, () => console.log("Listening to " + port));
